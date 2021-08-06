5b5465b38badd3eb900802199e1c075d849425d4
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2021-08-06 17:16:56 +0100

    Change input type and input order
    Take vector of datetimes as input to provide the 'angles' going into circstat's circ_rtest function, and make associated values optional for more generalizability of this function

--- circadian_rayleigh_test.m
+++ circadian_rayleigh_test.m
@@ -1,25 +1,48 @@
-function [pval, z] = circadian_rayleigh_test(circ_data_points, angles)
-% function [pval, z] = circadian_rayleigh_test(circ_data_points, angles)
+function [pval, z] = circadian_rayleigh_test(in_times, in_values)
+% function [PVAL, Z] = CIRCADIAN_RAYLEIGH_TEST(IN_TIMES, IN_VALUES)
+% 
+% Computes the Rayleigh test for non-uniformity of circadian data using the 
+% circstat toolbox ('circ_rtest').
+% 
+% INPUTS:
+% 
+% IN_TIMES: A vector of datetimes or durations specifying the times at
+% which events or measurements occurred.
+% 
+% IN_VALUES (optional): A vector of values equal in size to IN_TIMES, 
+% corresponding to the value / weight associated with each time point. If
+% no IN_VALUES are specified, each time point has a default weight of 1.
+% 
+% 
+% OUTPUTS:
+% 
+% PVAL: P-value representing the probability that the null hypothesis of
+% values being evenly distributed around the circle is true. 
+% 
+% Z: Value of the Z-statistic from the Rayleigh test.
+% 
+% Joram van Rheede, 2021
 
-n_angles    = size(circ_data_points,2);
+% Convert input datetimes to angles (in radians)
+time_angles         = datetimes_to_angles(in_times);
 
-% If no angles are specified
-if nargin == 1
-    % assume that circ_data_points are distributed over angles that evenly
-    % span a circle
-    angles = [1:n_angles]/n_angles*2*pi; 
+% If no values have been specified, give each angle a weight of 1
+if nargin < 2
+    in_values       = ones(size(time_angles));
 end
 
-% 
-angles              = repmat(angles, size(circ_data_points,1),1);
+% Vectorise circ_data_points and angles
+in_values           = in_values(:);
+time_angles      	= time_angles(:);
 
-% vectorise circ_data_points and angles
-circ_data_points    = circ_data_points(:);
-angles              = angles(:);
+% Remove any NaN values
+is_nan              = isnan(in_values);
 
-is_not_nan        	= ~isnan(circ_data_points);
+if any(is_nan)
+    in_values       = in_values(~is_nan);
+    time_angles   	= time_angles(~is_nan);
+end
 
-final_circ_points   = circ_data_points(is_not_nan);
-angles              = angles(is_not_nan);
-
-[pval,z]            = circ_rtest(angles, final_circ_points);
+% Perform Rayleigh test from circstat toolbox (see 'help circ_rtest' for 
+% details)
+[pval, z]           = circ_rtest(time_angles, in_values);
