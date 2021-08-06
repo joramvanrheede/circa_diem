4e95761090278405214488722d1295f85215cae2
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2021-08-06 12:51:18 +0100

    Use dedicated function for plot aesthetics
    ...and remove redundant code

--- circadian_rose.m
+++ circadian_rose.m
@@ -83,22 +83,7 @@ circadian_plot_points   = [circadian_plot_points(:); circadian_plot_points(1)];
 
 % get mean or median of all in_values at each 1h point
 plot_handle             = polarplot(circadian_plot_times(:),circadian_plot_points(:),'LineWidth',2);
-set(gca,'ThetaZeroLocation','top','ThetaDir','clockwise')
 
-% Plot aesthetics for polar axes
-set(gca,'LineWidth',2,'FontSize',16,'FontName','Arial','FontWeight','Bold','TickDir','out','box','off','ThetaColor',[0 0 0],'RColor',[0 0 0],'GridColor',[0 0 0])
-
-% Get the angles of the polar plot markers, convert to 24h clock values
-angular_ticks           = get(gca,'ThetaTick');
-circadian_ticks         = (angular_ticks / 360) * 24;
-
-% Replace the numeric labels with time strings in the format 'hh:mm'
-circadian_labels        = cell(size(circadian_ticks));
-for i = 1:length(circadian_ticks)
-    this_tick           = circadian_ticks(i);
-    circadian_labels(i) = {[num2str(this_tick,'%02.f') ':00']};
-end
-
-% Change the labels to 24h notation
-set(gca,'ThetaTickLabel', circadian_labels)
+% Function that adjusts the labels & aesthetics on circadian plots
+circadian_plot_aesthetics
 
