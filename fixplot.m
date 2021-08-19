60fbbce9125727a65a45164e003ef168f444ba84
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2021-08-19 15:55:43 +0100

    Add quick fix for cartesian plot axes

---
+++ fixplot.m
@@ -0,0 +1,12 @@
+function fixplot
+% function FIXPLOT
+% 
+% Quick aesthetics fix for cartesian axes plots in MATLAB. Edit to your
+% personal preferences.
+% 
+% Joram van Rheede, 2021
+
+% Edit plot settings to increase legibility and improve aesthetics
+set(gca,'LineWidth',2,'FontSize',16,'FontName','Helvetica','FontWeight','Bold','TickDir','out','box','off')
+
+
