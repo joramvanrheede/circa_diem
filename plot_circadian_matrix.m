486c6e26249dc0cb29875a36ea04aa2f5dca2eda
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2021-08-20 12:12:33 +0100

    Change long input list to single circadian matrix

--- plot_circadian_matrix.m
+++ plot_circadian_matrix.m
@@ -1,59 +1,35 @@
-function [circadian_matrix, time_edges] = plot_circadian_matrix(time_points, in_data, time_res, stat)
-% function [circadian_matrix, time_edges] = plot_circadian_matrix(time_points, in_data, time_res, stat)
+function im_handle = plot_circadian_matrix(circadian_matrix, percentile_cutoff)
+% function [circadian_matrix, time_edges] = plot_circadian_matrix(circadian_matrix)
 % 
-% Plot IN_DATA collected at a series of TIME_POINTS (datetimes) as a 
-% matrix with each row representing a day, and each column representing a 
-% time bin (width of time_bin controlled by TIME_RES, in hours).
+% Plot a circadian matrix generated with make_circadian_matrix. option to
+% cut off top and bottom percentiles to avoid effect of very small or large
+% values on the overall colormap.
 % 
 % Each point in the matrix will be the average (mean or median, controlled
 % by STAT) of all in_data points that were collected within that time bin.
 % 
 % INPUTS:
 %
-% TIME_POINTS: Datetime vector of time points corresponding to data points
-% in IN_DATA.
-%
-% IN_DATA: A vector of data values, with a corresponding value for each
-% time point in TIME_POINTS. Use NaNs for missing data, they will be
-% ignored.
-%
-% TIME_RES: The time resolution in hours, i.e. the size of the time bins 
-% for creating the entries in the circadian matrix. Defaults to 1 (hour),
-% dividing the day into 24 1-hour bins.
-%
-% STAT: Which statistic to use to generate each binned value. Default is 
-% 'mean', but for a more robust estimate 'median' can be used.
+% CIRCADIAN_MATRIX: A circadian matrix obtained via 'make_circadian_matrix'
 % 
 % 
 % OUTPUTS:
 % 
-% CIRCADIAN_MATRIX: An MxN matrix, where M is the number of days in
-% time_points, and N is the number of time bins (as determined by
-% TIME_RES).
-% 
-% TIME_EDGES: A duration vector of edges of the time bins around the 24h 
-% clock, with the duration between successive edges equal to TIME_RES.
+% IM_HANDLE: A handle to the image axes object.
 % 
 % 
 % Joram van Rheede, 2021
 
-if nargin < 3 || isempty(time_res)
-    time_res = 1;
+if nargin < 2
+    percentile_cutoff = 0;
 end
-
-if nargin < 4 || isempty(stat)
-    stat = 'mean';
-end
-
-% Make circadian matrix
-[circadian_matrix, time_edges] = make_circadian_matrix(time_points, in_data, time_res, stat);
 
 % Plot circadian matrix as scaled image
-imagesc(circadian_matrix)
+im_handle = imagesc(circadian_matrix);
 
 % Exclude top and bottom 2% from colour range to keep the colormap
 % informative despite outliers in the data
-set(gca,'CLim',prctile(circadian_matrix(:),[2 98]))
+set(gca,'CLim',prctile(circadian_matrix(:),[percentile_cutoff 100-percentile_cutoff]))
 
 % Add axis labels and set tick points
 ylabel('Days')
