205e81955b575d572888f4e400dd691565179413
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2021-08-20 13:00:57 +0100

    Take shuffled vector lengths and dirs as input

--- plot_shuffled_vectors.m
+++ plot_shuffled_vectors.m
@@ -1,15 +1,12 @@
-function [shuffled_vector_lengths, shuffled_vector_dirs, p_val] = plot_shuffled_vectors(time_points, in_data, n_shuffles, shuffle_mode)
-% function plot_shuffled_vectors(time_points, in_data, n_shuffles, shuffle_mode)
+function [plot_handle] = plot_shuffled_vectors(shuffled_vector_lengths, shuffled_vector_dirs)
+% [plot_handle] = plot_shuffled_vectors(shuffled_vector_lengths, shuffled_vector_dirs)
 % 
 % Plot shuffled circadian vectors
 % 
 % Joram van Rheede, 2021
 
-% Get shuffled vectors
-[shuffled_vector_lengths, shuffled_vector_dirs, p_val] = get_shuffled_vectors(time_points, in_data, n_shuffles, shuffle_mode);
-
 % polar scatter plot
-polarscatter(shuffled_vector_dirs, shuffled_vector_lengths,'ko','filled','MarkerFaceAlpha',.1, 'MarkerEdgeAlpha',.2)
+plot_handle = polarscatter(shuffled_vector_dirs, shuffled_vector_lengths,'ko','filled','MarkerFaceAlpha',.1, 'MarkerEdgeAlpha',.2);
 
 % Function with formatting defaults for circadian polar plots
 circadian_plot_aesthetics
