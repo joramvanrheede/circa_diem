813a54a5ae46056ce36f6f4719a48c41f8f66a2e
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2021-08-26 16:11:39 +0100

    Add optional input for font size and name

--- fixplot.m
+++ fixplot.m
@@ -1,12 +1,23 @@
-function fixplot
+function fixplot(font_size,font_name)
 % function FIXPLOT
 % 
-% Quick aesthetics fix for cartesian axes plots in MATLAB. Edit to your
-% personal preferences.
+% Quick aesthetics fix for cartesian axes plots in MATLAB. Edit defaults to 
+% your personal preferences.
 % 
 % Joram van Rheede, 2021
 
+% Default to Helvetica
+if nargin < 2
+    font_name = 'Helvetica';
+end
+
+% Default to font size appropriate for single plots; smaller font size may
+% be required if using many subplots
+if nargin < 1 || isempty(font_size)
+    font_size = 16;
+end
+
 % Edit plot settings to increase legibility and improve aesthetics
-set(gca,'LineWidth',2,'FontSize',16,'FontName','Helvetica','FontWeight','Bold','TickDir','out','box','off')
+set(gca,'LineWidth',2,'FontSize',font_size,'FontName',font_name,'FontWeight','Bold','TickDir','out','box','off')
 
 
