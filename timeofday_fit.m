364b62b18fb781bfb506592ca23f0c6c61aa107d
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2021-11-02 11:50:08 +0000

    Add optional time resolution input

--- timeofday_fit.m
+++ timeofday_fit.m
@@ -1,4 +1,4 @@
-function fit_obj = timeofday_fit(time_stamps, values)
+function fit_obj = timeofday_fit(time_stamps, values, time_res)
 % function fit_obj = timeofday_fit(time_stamps, values)
 % 
 % Produce a linear interpolated fit to generate an estimate for VALLES for
@@ -6,14 +6,16 @@ function fit_obj = timeofday_fit(time_stamps, values)
 % 
 % Joram van Rheede, 2021
 
-[time_bin_means, bin_edges] = circadian_means(time_stamps, values, 0.5);
+if nargin < 3
+    time_res = 0.5;
+end
 
+[time_bin_means, bin_edges] = circadian_means(time_stamps, values, time_res);
 
-bin_centres     = bin_edges - (0.5 * median(diff(bin_edges)));
+bin_edges       = hours(bin_edges);
+bin_centres     = bin_edges - 0.5 * time_res;
 
-bin_centres     = hours(bin_centres);
-
-bin_centres     = [bin_centres(:); bin_centres(end)+median(diff(bin_centres))];
+bin_centres     = [bin_centres(:); bin_centres(end) + time_res];
 
 % add first value at the end and first value at the beginning so fit covers
 % the whole 24h cycle (otherwise it would have to extrapolate for values
