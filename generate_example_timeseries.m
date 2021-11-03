2e72c15f280270b7cefe66921fafefded0dac84c
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2021-11-03 14:03:57 +0000

    Provide useful help text

--- generate_example_timeseries.m
+++ generate_example_timeseries.m
@@ -1,21 +1,57 @@
-function [time_stamps, values] = generate_example_timeseries(n_days, sample_freq, signal_perc)
-% function [time_stamps, values] = generate_example_timeseries(n_days, sample_freq, signal_perc, noise_perc)
+function [time_stamps, values] = generate_example_timeseries(n_days, sample_freq, signal_perc, smoothing)
+% function [time_stamps, values] = generate_example_timeseries(n_days, sample_freq, signal_perc, smoothing)
+% 
+% Basic simulation function to generate an example timeseries containing a 
+% sinusoidal circadian component within noise. Outputs regularly spaced
+% measurement TIME_STAMPS and associated simulated VALUES. 
+% 
+% By itself, GENERATE_EXAMPLE_TIMESERIES will generate 10 days worth of
+% data sampled at 2 samples/hour with 20% signal within 80% noise smoothed
+% with a moving average window of 5 samples. These properties can be
+% controlled with the input variables below.
+% 
+% INPUTS:
+% 
+% N_DAYS: How many days of data to simulate? Defaults to 10.
+% 
+% SAMPLE_FREQ: Frequency at which the data are sampled. Defaults to 2
+% samples / hour.
+% 
+% SIGNAL_PERC: Percentage of signal vs noise. Defaults to 20%.
+% 
+% SMOOTHING: Moving average window to smooth the data; this will
+% effectively decrease the frequency of the random component in the data
+% and introduce stronger correlations between successive time points.
+% Defaults to 5.
 % 
 % 
+% OUTPUTS:
 % 
+% TIME_STAMPS: The measurement times of the simulated data.
+% 
+% VALUES: The simulated measurement values associated with the TIME_STAMPS.
+% 
+% 
+% Circa Diem Toolbox 2021
 
+
+% Default smoothing to generate stronger local correlations between
+% successive time points
 if nargin < 4
     smoothing = 5;
 end
 
+% Signal percentage (within noise)
 if nargin < 3
     signal_perc = 20;
 end
 
+% Frequency (in samples/hour)
 if nargin < 2
     sample_freq = 2;
 end
 
+% Number of days for which to generate data
 if nargin < 1
     n_days = 10;
 end
