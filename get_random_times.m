f7d175519a463de85243b894232f4f09990245ec
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2021-11-11 15:26:06 +0000

    Add informative help

--- get_random_times.m
+++ get_random_times.m
@@ -1,10 +1,28 @@
 function random_times = get_random_times(start_time, end_time, n_events, n_repeats)
-% function random_times = get_random_times(start_time, end_time, n_events, n_repeats)
+% FUNCTION RANDOM_TIMES = GET_RANDOM_TIMES(START_TIME, END_TIME, N_EVENTS, N_REPEATS)
 % 
-% Generate random times between a start and an end time.
+% Generate random event times between a start and an end time.
 % 
-% Joram van Rheede, 2021
+% INPUTS:
+% 
+% START_TIME: Start time from which to generate events.
+% 
+% END_TIME: End time until which to generate events.
+%
+% N_EVENTS: How many events to generate between START_TIME and END_TIME.
+%
+% N_REPEATS: How many times to generate N_EVENTS random event times.
+% 
+% 
+% OUTPUT:
+% 
+% RANDOM_TIMES: A matrix of datetimes of size N_EVENTS * N_REPEATS 
+% containing random EVENT_TIMES between START_TIME and END_TIME.
+% 
+% 
+% Circa Diem Toolbox, 2021
 
+% By default, only generate random event times once
 if nargin < 4
     n_repeats = 1;
 end
