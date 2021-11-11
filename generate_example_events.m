b96fa48a297fecb07d1a45e7b44fc71c71b5e38b
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2021-11-11 15:15:08 +0000

    Fix help, remove redundant line, change default peak time

--- generate_example_events.m
+++ generate_example_events.m
@@ -1,5 +1,27 @@
-function event_times = generate_example_events(n_days, event_rate, center_time)
-% function generate_example_events(n_days, event_rate)
+function event_times = generate_example_events(n_days, event_rate, peak_time)
+% function EVENT_TIMES = GENERATE_EXAMPLE_EVENTS(N_DAYS, EVENT_RATE, PEAK_TIME)
+% 
+% Basic simulation function to generate example event times with a
+% frequency that varies sinusoidally across the course of the times of day.
+% 
+% By itself, GENERATE_EXAMPLE_EVENTS will generate 30 days worth of
+% events, with an average of 100 events per day normally distributed around
+% a peak at 12 noon.
+% 
+% INPUTS:
+% 
+% N_DAYS: How many days of data to simulate? Defaults to 30.
+% 
+% EVENT_RATE: How many events per day to generate? Defaults to 100.
+% 
+% PEAK_TIME: When in the day should events peak? Defaults to 12 noon. 
+% 
+% 
+% OUTPUTS:
+% 
+% EVENT_TIMES: The simulated event times.
+% 
+% Circa Diem Toolbox 2021
 
 if nargin < 1
     n_days = 30;
@@ -10,20 +32,17 @@ if nargin < 2
 end
 
 if nargin < 3
-    center_time = 17;
+    peak_time = 12;
 end
-
-% How many events in total
-n_events        = n_days * event_rate;
 
 event_times     = [];
 for a = 1:n_days
     events_numeric  = randn(event_rate, 1);
-    events_24h      = events_numeric * 24 + center_time;
+    events_24h      = events_numeric * 24 + peak_time;
     events_hours    = hours(events_24h);
 
 
-    events_24h      = events_numeric * 6 + center_time;
+    events_24h      = events_numeric * 6 + peak_time;
     events_hours    = hours(events_24h);
 
     % Convert duration in hours into datetime by adding dummy date (today)
