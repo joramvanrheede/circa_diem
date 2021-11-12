1698956399dcbb64cbaef472e782910a59841229
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2021-11-12 16:52:28 +0000

    Add informative help

--- circadian_periodogram.m
+++ circadian_periodogram.m
@@ -1,10 +1,38 @@
 function [psd_estimate, time_periods] = circadian_periodogram(time_stamps, signal, time_res, max_period)
-% function [psd_estimate, target_freqs] = circadian_periodogram(time_stamps, signal, time_res, max_period)
+% function [PSD_ESTIMATE, TARGET_FREQS] = CIRCADIAN_PERIODOGRAM(TIME_STAMPS, signal, time_res, max_period)
 % 
-% Calculates a circadian periodogram
+% Calculates a periodogram for timeseries SIGNAL collected at time points
+% TIME_STAMPS, for periods increasing in steps of TIME_RES up to a maximum 
+% period of MAX_PERIOD. 
+% 
+% The periodogram is essentially Welch's power spectral density estimate
+% for the frequency that is the inverse of each time period. TIME_RES, 
+% MAX_PERIOD, and output variable TIME_PERIODS are specified in units of 
+% hours.
 % 
 % 
+% INPUTS:
 % 
+% TIME_STAMPS: A vector of datetimes corresponding to the times at which
+% SIGNAL was measured.
+% 
+% SIGNAL: The measurement values corresponding to TIME_STAMPS.
+% 
+% TIME_RES: The time resolution at which to calculate the periodogram in 
+% hours. Defaults to 1 (hour).
+% 
+% MAX_PERIOD: The maximum time period for which to calculate the power
+% spectral density.
+% 
+% OUTPUTS:
+% 
+% PSD_ESTIMATE: Power spectral density estimates obtained using Welch's 
+% methods, for an increasing series of time periods.
+% 
+% TIME_PERIODS: The time periods for which PSD was calculated, in hours.
+% 
+% 
+% Circa Diem Toolbox 2021
 
 % Default to 1 hour time resolution
 if nargin < 3 || isempty(time_res)
