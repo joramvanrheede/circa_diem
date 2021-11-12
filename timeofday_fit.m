d2a01ad4e353c5febd59011e8922b48e0b6a016c
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2021-11-12 16:51:18 +0000

    Add informative help, fix time res input

--- timeofday_fit.m
+++ timeofday_fit.m
@@ -1,25 +1,52 @@
 function fit_obj = timeofday_fit(time_stamps, values, time_res)
-% function fit_obj = timeofday_fit(time_stamps, values)
+% function FIT_OBJ = TIMEOFDAY_FIT(TIME_STAMPS, VALUES, TIME_RES)
 % 
-% Produce a linear interpolated fit to generate an estimate for VALLES for
-% any time of day.
+% Make a linear interpolated fit to generate an estimate for VALUES for
+% any time of day based on the mean value for that time. The estimates are
+% calculated for time bins of size by TIME_RES (in hours), and the overall
+% fit is a linear interpolation between the bin means for the different bin
+% centres.
 % 
-% Joram van Rheede, 2021
+% INPUTS: 
+% 
+% TIME_STAMPS: A vector of datetimes corresponding to the times at which
+% VALUES were measured.
+% 
+% VALUES: The measurement values corresponding to TIME_STAMPS.
+% 
+% TIME_RES: The size of the time bins in hours. /!\ For sensible results, 
+% this value should be greater than the sampling interval of the data. /!\
+% Defaults to 1.
+% 
+% OUTPUTS:
+% 
+% FIT_OBJ: A MATLAB fit object that can be used to retrieve values for any
+% give time of day (specified numerically in hours).
+% 
+% 
+% Circa Diem Toolbox 2021
 
+% Default time resolution is half an hour
 if nargin < 3
     time_res = 0.5;
 end
 
+% Get the means values in the time bins
 [time_bin_means, bin_edges] = circadian_means(time_stamps, values, time_res);
 
+% Convert numeric bin_edges to hours
 bin_edges       = hours(bin_edges);
+
+% Calculate the bin centres which are needed for the fit
 bin_centres     = bin_edges - 0.5 * time_res;
 
+% Add an extra bin centre such that the first bin centre extends below
+% 00:00 and the final bin centre extends above 24:00 so that there are no
+% out of range values
 bin_centres     = [bin_centres(:); bin_centres(end) + time_res];
 
 % add first value at the end and first value at the beginning so fit covers
-% the whole 24h cycle (otherwise it would have to extrapolate for values
-% between 00:00 - 00:30 and 23:30 - 24:00
+% the whole 24h cycle 
 time_bin_means  = [time_bin_means(end); time_bin_means(:); time_bin_means(1)];
 
 % Fill empty values with neighbouring ones
--- variance_explained_by_timeofday.m
+++ variance_explained_by_timeofday.m
@@ -1,13 +1,43 @@
-function [var_explained, pre_var, post_var] = variance_explained_by_timeofday(time_stamps, values)
-% function variance_explained_by_timeofday(time_stamps, values)
+function [var_explained, pre_var, post_var] = variance_explained_by_timeofday(time_stamps, values, time_res)
+% function [VAR_EXPLAINED, PRE_VAR, POST_VAR] = VARIANCE_EXPLAINED_BY_TIMEOFDAY(TIME_STAMPS, VALUES, TIME_RES)
 % 
-% Calculate the amount of variance explained by time of day
+% Calculate the proportion of variance explained by a fit to the data based
+% on the means for particular times of day. The time-of-day fit is 
+% calculated for time bins of size by TIME_RES (in hours), and the overall 
+% fit is a linear interpolation between the bin means for the different bin 
+% centres (see TIMEOFDAY_FIT).
 % 
-% Joram van Rheede, 2021
+% INPUTS: 
+% 
+% TIME_STAMPS: A vector of datetimes corresponding to the times at which
+% VALUES were measured.
+% 
+% VALUES: The measurement values corresponding to TIME_STAMPS.
+% 
+% TIME_RES: The size of the time bins in hours. /!\ For sensible results, 
+% this value should be greater than the sampling interval of the data. /!\
+% Defaults to 1.
+% 
+% OUTPUTS:
+% 
+% VAR_EXPLAINED: Proportion of variance explained.
+% 
+% PRE_VAR: Total variance in VALUES
+% 
+% POST_VAR: Variance after the estimates from the time of day fit have been
+% subtracted.
+% 
+% 
+% Circa Diem Toolbox 2021
+
+% If not specified, let time_res default to 1 (hour)
+if nargin < 3
+    time_res = 1;
+end
 
 % Create linearly interpolated fit object to obtain a predicted value for
 % each time of day
-fit_obj                 = timeofday_fit(time_stamps, values);
+fit_obj                 = timeofday_fit(time_stamps, values, time_res);
 
 % Convert datetimes to numeric time of day in hours (between 0 and 24)
 time_stamps_numeric     = hours(timeofday(time_stamps));
