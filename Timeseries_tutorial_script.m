7bceafac5d124e6a72bec8ab6e06c32a1d0a86a9
Joram van Rheede <joramvanrheede@gmail.com>
2021-11-16 09:37:04 +0000

    Add circadian matrix heatmap; circadian vector, etc

--- Timeseries_tutorial_script.m
+++ Timeseries_tutorial_script.m
@@ -15,6 +15,8 @@
 % 
 % Circa Diem Toolbox 2021
 
+my_favourite_colour     = [0.8500 0.3250 0.0980]; % change to your personal favourite
+
 %% 0: Generate an example timeseries to work with
 
 n_days          = 30; % Number of days to simulate
@@ -24,34 +26,82 @@ smoothing       = 3; % Smoothing window for moving average smoothing (in samples
 
 [time_stamps, values]   = generate_example_timeseries(n_days, sample_freq, signal_perc, smoothing);
 
+
+
 %% 2: Visualise the basic data set 
 
 figure
 set(gcf,'Units','Normalized','Position',[.2 .4 .6 .25])
-plot_zscored_timeseries(time_stamps, values)
+plot_zscored_timeseries(time_stamps, values, my_favourite_colour)
+
+
 
 %% 3: Visualise circadian periodicity in the data
 
 time_res        = 1; % time resolution (in hours) for periodogram
 max_period      = 72; % Maximum period of 1 week = 168 hours
+do_normalise    = true; % Whether to normalise the periodogram
 
 % Calculate periodogram
 [psd_estimate, time_periods] = circadian_periodogram(time_stamps, values, time_res, max_period);
 
-
-do_normalise    = true;
-
 % Plot the periodogram
 figure
 set(gcf,'Units','Normalized','Position',[.3 .4 .4 .2])
-plot_periodogram(psd_estimate, time_periods, do_normalise)
+plot_periodogram(psd_estimate, time_periods, do_normalise, my_favourite_colour)
+
+
 
 %% 4: Calculate and visualise variance explained by time of day
 % *Currently requires curve fitting toolbox
 
+time_res        = 1;
+n_shuffles      = 1000;
+shuffle_type    = 'circshift';
+
+% Get the proportion of variance explained by time of day
+var_explained = variance_explained_by_timeofday(time_stamps, values, time_res);
+
+% Get the variance explained for shuffled data n_shuffles times to see whether var_explained is significant
+[shuffled_var_explained, var_explained_p] = get_shuffled_var_explained(time_stamps, values, time_res, n_shuffles, shuffle_type);
+
+% Plot a fit based on time of day to the data across days
+figure
+plot_timeofday_fit(time_stamps, values, time_res, my_favourite_colour)
+
+% Add the variance explained by time of day & p-val to the figure title
+title(['Var explained by TOD: ' num2str(var_explained) ', p =' num2str(var_explained_p)])
+
+
+
+%% 5: Plot circadian rose plot
 time_res    = 1;
+stat        = 'mean';
 
-variance_explained_by_timeofday(time_stamps, values, time_res)
+figure
+circadian_rose(time_stamps, values, time_res, stat, my_favourite_colour)
 
+
+%% 6: Make heatmap of
+
+time_res            = 1; % Temporal resolution of time bins (= heatmap pixels)
+percentile_cutoff   = 2; % For the colour scale of the heatmap, ignore the top and bottom x% of data
+
+[circadian_matrix, time_edges] = make_circadian_matrix(time_stamps, values, time_res);
+
+figure
+plot_circadian_matrix(circadian_matrix, percentile_cutoff, my_favourite_colour);
+
+
+
+%% 6: Calculate circadian vector
+% *uses the circstat toolbox
+
+[vector_length, vector_dir] = circadian_vect(time_stamps, values);
+
+[shuffled_vector_lengths, shuffled_vector_dirs] = get_shuffled_vectors(time_stamps, values);
+
+figure
+plot_circadian_vector(vector_length, vector_dir, my_favourite_colour);
 
 
