50de1a7e588fcd8a212281e9f9aa7c0ea9365e37
Joram van Rheede <joramvanrheede@users.noreply.github.com>
2022-02-28 14:25:17 +0000

    Add polar plot option

--- plot_timeofday_fit.m
+++ plot_timeofday_fit.m
@@ -1,4 +1,4 @@
-function plot_timeofday_fit(time_stamps, values, time_res, colour)
+function plot_timeofday_fit(time_stamps, values, time_res, colour, plot_type)
 % function PLOT_TIMEOFDAY_FIT(TIME_STAMPS, VALUES, TIME_RES, COLOUR)
 %
 % Plot a time of day fit
@@ -16,6 +16,10 @@ if nargin < 4
     colour  = [0 0 0];
 end
 
+if nargin < 5
+    plot_type = 'cartesian';
+end
+
 % Plot the fit minute by minute
 fit_times   = hours(0:(1/60):24);
 
@@ -25,11 +29,20 @@ timeofday_numeric   = hours(timeofday(time_stamps));
 
 %fit_values          = fit_obj(timeofday_numeric);
 
-scatter(timeofday(time_stamps),values,8,colour,'filled','MarkerFaceAlpha',0.5,'MarkerEdgeAlpha',0.5)
-hold on
-plot(fit_times,fit_obj(hours(fit_times)),'k-','LineWidth',2)
-xlim(hours([0 24]))
-fixplot(12)
-title('Time-of-day fit')
-ylabel('Value')
-xlabel('Time of day')
+switch plot_type
+    case 'cartesian'
+        scatter(timeofday(time_stamps),values,8,colour,'filled','MarkerFaceAlpha',0.5,'MarkerEdgeAlpha',0.5)
+        hold on
+        plot(fit_times,fit_obj(hours(fit_times)),'k-','LineWidth',2)
+        xlim(hours([0 24]))
+        fixplot(12)
+        title('Time-of-day fit')
+        ylabel('Value')
+        xlabel('Time of day')
+    case 'polar'
+        polarscatter(datetimes_to_angles(time_stamps),values, [],colour,'o','filled','MarkerFaceAlpha',0.3,'MarkerEdgeAlpha',0.3)
+        hold on
+        polarplot(datetimes_to_angles(fit_times),fit_obj(hours(fit_times)),'k-','LineWidth',2)
+        circadian_plot_aesthetics(12)
+        title('Time-of-day fit')
+end
